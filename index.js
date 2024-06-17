bce6454d1fa6b1c8cc8cfdece859abc503af61b6
chenyh <chenyh.site@gmail.com>
2024-06-17 15:45:21 +0800

    chore: build

---
+++ index.js
@@ -0,0 +1,8 @@
+function render() {
+  console.log("ci example");
+  const rendered = document.createElement("div");
+  rendered.innerText = "Hello World";
+  document.body.append(rendered);
+}
+
+window.addEventListener("DOMContentLoaded", render);
