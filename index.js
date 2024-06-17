480e8c00b1a119f44ee910e370343b20b7f78241
chenyh <chenyh.site@gmail.com>
2024-06-17 15:59:00 +0800

    chore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function render() {
   console.log("ci example");
   const rendered = document.createElement("div");
-  rendered.innerText = "Hello World";
+  rendered.innerText = "Hello World!";
   document.body.append(rendered);
 }
 
